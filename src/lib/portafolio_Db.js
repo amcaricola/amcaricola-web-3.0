9ea8bd3adf41abf3c91eb50a7f30d56b308105c1
Anthony M. Caricola <amcaricola@gmail.com>
2025-02-27 02:22:34 -0300

    v20250227
    nueva web con framework de astro, actualizacion para escalado.

---
+++ src/lib/portafolio_Db.js
@@ -0,0 +1,39 @@
+// base de datos de los portfolios
+const Portfolio_full = [
+  {
+    title: "AkalaEstampa.cl",
+    alt: "AkalaEstampa.cl",
+    img: "/portafolio/akalaestampa.png",
+    link: "https://akalaestampa.cl",
+    year: 2025,
+    description: "(WordPress - Woocommerce - CSS - javascript)", // agregar descripcion
+  },
+  {
+    title: "OCR - Reconocimiento de texto en imágenes",
+    alt: "OCR - Reconocimiento de texto en imágenes",
+    img: "/portafolio/OCR.png",
+    link: "https://www.amcaricola.com/OCR",
+    year: 2024,
+    description: "(WordPress - Woocommerce - CSS - javascript)", // agregar descripcion
+  },
+  {
+    title: "Amcaricola.com",
+    alt: "Amcaricola.com",
+    img: "/portafolio/amcaricolaWeb.png",
+    link: "https://www.amcaricola.com",
+    year: 2023,
+    description:
+      "Web de portafolio - Para mostrar y recopilar todos los proyectos y mostrar a clientes mi trabajo - Este enlace lleva al repositorio de Github de esta web (Astro Js - CSS - Javacript)",
+  },
+  {
+    title: "ArriendaAhora.cl",
+    alt: "ArriendaAhora.cl",
+    img: "/portafolio/arriendaAhora.png",
+    link: "https://arriendaahora.cl/",
+    year: 2021,
+    description:
+      "Web diseñada para ArriendaAhora.cl dedicado a compra, venta y arriendo de inmuebles, la web se encarga de mostrar el catálogo disponible (WordPress - CSS - javascript)",
+  },
+];
+
+export { Portfolio_full };
